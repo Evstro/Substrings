0745d6eb54ded65b16ac43701e1b7659f51103fe
Evstro <Evstro.code@gmail.com>
2022-09-13 16:07:56 -0500

    Complete
    Program seems to be functioning as intended
    
    [Ticket: X]

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,23 @@
 # methods
 def substrings(string, dictionary)
 
+  matching_substrings = Array.new
+  string_into_array = string.split
+
+  string_into_array.each do |substring|
+    dictionary.each do |string|
+      if substring.downcase.include?(string.downcase) == true
+        matching_substrings.push(string)
+      end      
+    end
+  end
+
+  matching_substrings_hash = matching_substrings.reduce(Hash.new(0)) do |string, times|
+    string[times] += 1
+    string
+  end
+
+ 
 end
 
 # variables
@@ -8,3 +25,7 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 # program
 substrings("below", dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+substrings("it it it it it", dictionary)
