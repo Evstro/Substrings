2edeaeae3c184366789a1a05d2494994bda0f7d1
Evstro <Evstro.code@gmail.com>
2022-09-13 15:35:42 -0500

    Set Up
    Added text to README.md and set up substrings.rb
    
    [Ticket: X]

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Substrings
+
+This program will take a string and use an array of strings and find what strings
+from the array of strings are contained within the initial string
---
+++ substrings.rb
@@ -0,0 +1,10 @@
+# methods
+def substrings(string, dictionary)
+
+end
+
+# variables
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+# program
+substrings("below", dictionary)
